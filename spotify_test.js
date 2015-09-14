465cd30e0dd4461243f107afdf7087f600c66cec
babyshoes <dianecai@gmail.com>
2015-09-14 14:53:03 -0400

    all done

---
+++ spotify_test.js
@@ -0,0 +1,10 @@
+alert("hi");
+
+$.ajax({
+  url:  "http://charts.spotify.com/api/tracks/most_streamed/us/daily/latest",
+  method: "GET",
+  dataType: "JSONP"
+}).success(function( spotifyData ) {
+  var songTitle = spotifyData.tracks[0].firstTrack.track_name;
+  console.log(songTitle);
+});
